8cdaceef680280ca8e15e5c752732da407116c86
sora <834037406@qq.com>
2023-10-07 11:13:47 +0800

    fix: 提交UserAddDto

---
+++ model/src/main/java/com/yyh/amailsite/acl/model/user/dto/UserAddDto.java
@@ -0,0 +1,19 @@
+package com.yyh.amailsite.acl.model.user.dto;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class UserAddDto {
+    @NonNull
+    @Size(min = 4, max = 16, message = "用户名长度为4-16")
+    @Pattern(regexp = "[A-Za-z0-9]+", message = "只能包含字母和数字")
+    private String username;
+    @NonNull
+    @Size(min = 4, max = 16, message = "密码长度为4-16")
+    @Pattern(regexp = "[A-Za-z0-9]+", message = "只能包含字母和数字")
+    private String password;
+}
